f9a6462c3bb1b2541db599835c6530aad8ece30c
Felipe Sousa <felipzbr@gmail.com>
2016-03-16 14:47:42 -0300

    v0.1.0

---
+++ README.md
@@ -0,0 +1,336 @@
+# Wunderlist-api
+> :closed_book: Wunderlist in a simple way.
+
+## Installation
+`[sudo] npm install --save-dev wunderlist-api`
+
+## Docs
+  - [Authorization](#authorization)
+  - [User](#user)
+    - [User Informations](#user-information)
+    - [Avatar´s](#avatar)
+  - [Lists](#lists)
+    - [Get all lists](#get-all-lists)
+    - [Get specified list](#get-specified-list)
+    - [Make a list from state (public or private)](##make-a-list-from-state-public-or-private)
+    - [Create list](#create-list)
+    - [Update list](#update-list)
+    - [Delete list](#delete-list)
+    - [Users authorized in list](#users-authorized-in-list)
+  - [Notes](#notes)
+    - [Get note from list](##get-notes-from-list)
+    - [Get note from task](#get-notes-from-task)
+    - [Create Note](#create-note)
+    - [Delete Note](#delete-note)
+  - [Tasks](#tasks)
+    - [Get all tasks](#get-all-tasks)
+    - [Get task for state](#get-tasks-for-state)
+    - [Get specified task](#get-specified-task)
+    - [Create task](#create-task)
+    - [Delete task](#delete-task)
+  - [Subtasks](#subtasks)
+    - [Get subtask from list](#get-subtask-from-list)
+    - [Get completed subtasks from list](#get-completed-subtasks-from-list)
+    - [Get subtask from task](#get-subtask-from-task)
+    - [Get completed subtask from task](#get-completed-subtask-from-task)
+    - [Create subtask](#create-subtask)
+    - [Delete subtask](#delete-subtask)
+  - [Task Comments](#task-comments)
+    - [Get comments from Task](#get-comments-from-task)
+    - [Get comments from Lists](#get-comments-from-lists)
+    - [Create Comment](#create-comment)
+  - [Membership](#membership)
+    - [Get Membership](#get-membership)
+    - [Add member to list](#add-member-to-list)
+    - [Remove a Member from a List](#remove-a-member-from-a-list)
+
+
+## Concepts
+
+There are some parameters that are needed to operate certain methods, among all, some are rather odd, such as the `revision`, that is the amount of change that this task note, user, list, comment, subtask, in short, anything was modified, by default, it comes with the value `1`. When required this parameter, you must enter the value of the `current revision` and not the following number. For example, to delete a user from the list that was newly created, I pass the current value of revision in case the value `1`. 
+For more information on types of parameters or any other matter, please visit the [official site Wunderlist](https://developer.wunderlist.com/documentation).
+
+
+## Usage
+
+#### Authorization
+  To access the information have 2 data, the first the access token and the Client ID, this information is [available here.](https://developer.wunderlist.com/apps)
+
+  ```javascript
+  var wunderlist = require('wunderlist-api');
+
+  wunderlist.authentication(accessToken, clientID);
+  // => authenticated
+  ```
+
+> All the following methods may be accessed in the same way, only changing the parameter when it becomes necessary, show only an example that can be repeated after any other. The method is based on the concept of [promises](https://spring.io/understanding/javascript-promises).
+
+Universal Exampler: *for all methods is REQUIRED use `.then()`  for get response!*
+
+```javascript
+var wunderlist = require('wunderlist-api');
+wunderlist.method(parameters)
+  .then(function (response){
+    // response
+  })
+  .catch(function (error){
+    // handle error
+  });
+```
+
+
+#### User
+
+##### User information
+
+```javascript
+wunderlist.user()
+```
+
+##### Avatar
+- user_id: integer
+- size: integer (optional)
+- fallback: boolean (optional)
+
+```javascript
+wunderlist.avatar(user_id, size, fallback)
+```
+
+#### Lists
+
+##### Get All lists
+```javascript
+wunderlist.getLists()
+```
+
+##### Get specified list
+- list_id: integer
+
+```javascript
+wunderlist.getOneList(list_id)
+```
+
+##### Make a list from state (public or private)
+- list_id: integer
+- revision: integer
+- public: boolean
+
+```javascript
+wunderlist.avatar(list_id, revision, public)
+```
+
+##### Create List
+- title: string
+
+```javascript
+wunderlist.createList(title)
+```
+
+##### Update List
+- list_id: integer
+- revision: integer
+- title: string
+
+```javascript
+wunderlist.updateList(list_id, revision, title)
+```
+
+##### Delete List
+- list_id: integer
+- revision: integer
+
+```javascript
+wunderlist.deleteList(list_id, revision)
+```
+
+##### Users authorized in list
+- list_id: integer
+
+```javascript
+wunderlist.listUsers(list_id)
+```
+
+#### Notes
+
+##### Get notes from list
+- list_id: integer
+
+```javascript
+wunderlist.getNoteFromList(list_id)
+```
+
+##### Get notes from task
+- task_id: integer
+
+```javascript
+wunderlist.getNoteFromTask(task_id)
+```
+
+##### Create note
+- task_id: integer
+- content: string
+
+```javascript
+wunderlist.createNote(task_id, content)
+```
+
+##### Delete note
+- note_id: integer
+- revision: integer
+
+```javascript
+wunderlist.deleteNote(note_id, revision)
+```
+
+#### Tasks
+
+##### Get all tasks
+- list_id: integer
+
+```javascript
+wunderlist.getTasks(list_id)
+```
+
+##### Get tasks for state
+- list_id: integer
+- state: boolean (completed or incompleted)
+
+```javascript
+wunderlist.getTasksForState(list_id, state)
+```
+
+##### Get specified task
+- task_id: integer
+
+```javascript
+wunderlist.getOneTask(task_id)
+```
+
+##### Create task
+- list_id: integer
+- title: string
+- state: boolean
+- starred: boolean
+
+```javascript
+wunderlist.createTask(list_id, title, state, starred)
+```
+
+##### Delete Task
+- task_id: integer
+- revision: integer
+
+```javascript
+wunderlist.deleteTask(task_id, revision)
+```
+
+#### Subtasks
+
+##### Get subtask from list
+- task_id: integer
+
+```javascript
+wunderlist.getSubtaskFromList(task_id)
+```
+
+##### Get completed subtasks from list
+- list_id: integer
+- completed: boolean
+
+```javascript
+wunderlist.getSubtaskCompletedFromList(list_id, completed)
+```
+
+##### Get subtask from task
+- task_id: integer
+
+```javascript
+wunderlist.getSubtaskFromTask(task_id)
+```
+
+##### Get completed subtask from task
+
+- task_id: integer
+- completed: boolean
+
+```javascript
+wunderlist.getSubtaskCompletedFromTask(task_id, completed)
+```
+
+##### Create subtask
+- task_id: integer
+- title: string
+- completed: boolean
+
+```javascript
+wunderlist.createSubstask(task_id, title, completed)
+```
+
+##### Delete subtask
+- subtask_id: integer
+- revision: integer
+
+```javascript
+wunderlist.deleteSubtask(subtask_id, revision)
+```
+
+
+#### Task Comments
+
+##### Get comments from Task
+- task_id: integer
+
+```javascript
+wunderlist.getCommentsTask(task_id)
+```
+
+##### Get comments from Lists
+- list_id: integer
+
+```javascript
+wunderlist.getCommentsList(list_id)
+```
+
+##### Create Comment
+- task_id: integer
+- text: string
+
+```javascript
+wunderlist.createComment(task_id, text)
+```
+
+#### Membership
+
+##### Get Membership
+
+```javascript
+wunderlist.getMembership()
+```
+
+##### Add member to list
+- user_id: integer
+- list_id: string
+- muted: boolean
+
+```javascript
+wunderlist.addMemberToList(user_id, list_id, muted)
+```
+
+##### Remove a Member from a List
+- user_id: integer
+- revision: integer
+
+```javascript
+wunderlist.removeMember(user_id, revision)
+```
+
+## Contributing
+- Fork it!
+- Create your feature branch: `git checkout -b my-new-feature`
+- Commit your changes: `git commit -am "Add some feature"`
+- Push to the branch: `git push origin my-new-feature`
+- Submit a pull request :D
+
+For more information on types of parameters or any other matter, please visit the [official site Wunderlist](https://developer.wunderlist.com/documentation).
+
+[License MIT](https://github.com/felipesousa/wunderlist-api/blob/master/LICENSE) @ 2016 Felipe Sousa
---
+++ index.js
@@ -0,0 +1,773 @@
+/*
+ * Wunderlist-api v0.1.0
+ * https://github.com/felipesousa/wunderlist-api
+ *
+ * MIT license @ 2016 Felipe Sousa
+ * https://github.com/felipesousa/wunderlist-api/blob/master/LICENSE
+ *
+ * Date: 2016-03-16
+ */
+
+var request = require('request');
+var _       = require('underscore');
+var keys    = {};
+var q       = require('q');
+
+// Headers
+exports.authentication = function (accessToken, clientId){
+  return keys = {
+    headers: {
+      'X-Access-Token': accessToken,
+      'X-Client-ID': clientId,
+      'Content-Type': 'application/json'
+    }
+  }
+}
+
+///////////// LISTS METHODS /////////////
+
+
+// GET all lists
+exports.getLists = function (){
+  var deferred = q.defer();
+  var url = {
+    url: 'https://a.wunderlist.com/api/v1/lists',
+    method: 'GET'
+  }
+  var options = _.extend(url, keys);
+
+  request(options, function(err, response, body){
+    if (err) {
+      deferred.reject(err);
+    }
+
+    if (response) {
+      deferred.resolve(response)
+    }
+  });
+
+  return deferred.promise;
+};
+
+// GET specified list
+exports.getOneList = function (list_id){
+  var deferred = q.defer();
+  var url = {
+    url: 'https://a.wunderlist.com/api/v1/lists/' + list_id,
+    method: 'GET'
+  }
+
+  var options = _.extend(url, keys);
+
+  request(options, function(err, response, body){
+    if (err) {
+      deferred.reject(err);
+    }
+
+    if (response) {
+      deferred.resolve(response)
+    }
+  });
+
+  return deferred.promise;
+}
+
+// Create a list
+exports.createList = function (title){
+  var deferred = q.defer();
+  var data = {
+    url: 'https://a.wunderlist.com/api/v1/lists',
+    method: 'POST',
+    json: {
+      "title": title
+    }
+  }
+
+  var options = _.extend(data, keys);
+
+  request(options, function(err, response, body){
+      if (err) {
+        deferred.reject(err);
+      }
+
+      if (response) {
+        deferred.resolve(response)
+      }
+    });
+
+    return deferred.promise;
+}
+
+// Update a List
+exports.updateList = function (list_id, revision, title){
+  var deferred = q.defer();
+  if (!_.isNumber(arguments[1])) {
+    throw new Error('Argument REVISION require type Number.');
+  }
+
+  var data = {
+    url: 'https://a.wunderlist.com/api/v1/lists/' + list_id,
+    method: 'PATCH',
+    json: {
+      "revision": revision,
+      "title": title
+    }
+  }
+
+  var options = _.extend(data, keys);
+
+  request(options, function(err, response, body){
+      if (err) {
+        deferred.reject(err);
+      }
+
+      if (response) {
+        deferred.resolve(response)
+      }
+    });
+
+    return deferred.promise;
+}
+
+// Public state List
+exports.stateList = function (list_id, revision, public){
+  var deferred = q.defer();
+  if (!_.isNumber(arguments[1])) {
+    throw new Error('Argument REVISION require type Number.');
+  }
+
+  var data = {
+    url: 'https://a.wunderlist.com/api/v1/lists/' + list_id,
+    method: 'PATCH',
+    json: {
+      "revision": revision,
+      "public": public
+    }
+  }
+
+  var options = _.extend(data, keys);
+
+  request(options, function(err, response, body){
+      if (err) {
+        deferred.reject(err);
+      }
+
+      if (response) {
+        deferred.resolve(response)
+      }
+    });
+
+    return deferred.promise;
+}
+
+exports.deleteList = function (list_id, revision){
+  var deferred = q.defer();
+  if (!_.isNumber(arguments[1])) {
+    throw new Error('Argument REVISION require type Number.');
+  }
+
+  var data = {
+    url: 'https://a.wunderlist.com/api/v1/lists/' + list_id + '?revision=' + revision,
+    method: 'DELETE'
+  }
+
+  var options = _.extend(data, keys);
+
+  request(options, function(err, response, body){
+      if (err) {
+        deferred.reject(err);
+      }
+
+      if (response) {
+        deferred.resolve(response)
+      }
+    });
+
+    return deferred.promise;
+}
+
+exports.listUsers = function (list_id){
+  var deferred = q.defer();
+  var data = {
+    url: 'https://a.wunderlist.com/api/v1/users',
+    method: 'GET'
+  }
+
+  var options = _.extend(data, keys);
+
+  request(options, function(err, response, body){
+      if (err) {
+        deferred.reject(err);
+      }
+
+      if (response) {
+        deferred.resolve(response)
+      }
+    });
+
+    return deferred.promise;
+}
+
+///////////// END: LIST METHODS /////////////
+
+/////////////// TASK METHODS ////////////////
+
+exports.getAllTasks = function (id){
+  var deferred = q.defer();
+  var url = {
+    url: 'https://a.wunderlist.com/api/v1/tasks?list_id=' + id,
+    method: 'GET'
+  }
+
+  var options = _.extend(url, keys);
+  request(options, function(err, response, body){
+    if (err) {
+      deferred.reject(err);
+    }
+
+    if (response) {
+      deferred.resolve(response)
+    }
+  });
+
+  return deferred.promise;
+}
+
+exports.getTasksForState = function (id, state){
+  var deferred = q.defer();
+  var url = {
+    url: 'https://a.wunderlist.com/api/v1/tasks?list_id=' + id + '&completed=' + state,
+    method: 'GET'
+  }
+
+  var options = _.extend(url, keys);
+  request(options, function(err, response, body){
+    if (err) {
+      deferred.reject(err);
+    }
+
+    if (response) {
+      deferred.resolve(response)
+    }
+  });
+
+  return deferred.promise;
+}
+
+exports.getOneTask = function (id){
+  var deferred = q.defer();
+  var url = {
+    url: 'https://a.wunderlist.com/api/v1/tasks/' + id,
+    method: 'GET'
+  }
+
+  var options = _.extend(url, keys);
+  request(options, function(err, response, body){
+    if (err) {
+      deferred.reject(err);
+    }
+
+    if (response) {
+      deferred.resolve(response)
+    }
+  });
+
+  return deferred.promise;
+}
+
+exports.createTask = function (list_id, title, state, starred){
+  var deferred = q.defer();
+  var url = {
+    url:  'https://a.wunderlist.com/api/v1/tasks',
+    method: 'POST',
+    json: {
+      "list_id": list_id,
+      "title": title,
+      "completed": state,
+      "starred": starred
+    }
+  }
+
+  var options = _.extend(url, keys);
+
+  request(options, function(err, response, body){
+    if (err) {
+      deferred.reject(err);
+    }
+
+    if (response) {
+      deferred.resolve(response)
+    }
+  });
+
+  return deferred.promise;
+}
+
+exports.deleteTask = function (id, revision){
+  var deferred = q.defer();
+  var url = {
+    url:  'https://a.wunderlist.com/api/v1/tasks/' + id + '?revision=' + revision,
+    method: 'DELETE',
+  }
+
+  var options = _.extend(url, keys);
+  request(options, function(err, response, body){
+    if (err) {
+      deferred.reject(err);
+    }
+
+    if (response) {
+      deferred.resolve(response)
+    }
+  });
+
+  return deferred.promise;
+}
+
+///////////// END: TASK METHODS /////////////
+
+/////////////// USERS METHODS ////////////////
+
+exports.user = function (){
+  var deferred = q.defer();
+  var url = {
+    url:  'https://a.wunderlist.com/api/v1/user',
+    method: 'GET',
+  }
+
+  var options = _.extend(url, keys);
+  request(options, function(err, response, body){
+    if (err) {
+      deferred.reject(err);
+    }
+
+    if (response) {
+      deferred.resolve(response)
+    }
+  });
+
+  return deferred.promise;
+}
+
+
+exports.avatar = function (user_id, size, fallback){
+
+  var  key= {
+    headers: {
+      'Content-Type': 'image/png'
+    }
+  }
+  var deferred = q.defer();
+  var url = {
+    url:  'https://a.wunderlist.com/api/v1/avatar?user_id=' + user_id + '&size=' + size + '&fallback=' + fallback,
+    method: 'GET',
+  }
+
+  var options = _.extend(url, key);
+  request(options, function(err, response, body){
+    if (err) {
+      deferred.reject(err);
+    }
+
+    if (response) {
+      deferred.resolve(response.request.href)
+    }
+  });
+
+  return deferred.promise;
+}
+
+
+///////////// END: USER /////////////
+
+/////////////// MEMBERSHIP ////////////////
+
+exports.getMembership = function (){
+  var deferred = q.defer();
+  var url = {
+    url:  'https://a.wunderlist.com/api/v1/memberships',
+    method: 'GET',
+  }
+
+  var options = _.extend(url, keys);
+  request(options, function(err, response, body){
+    if (err) {
+      deferred.reject(err);
+    }
+
+    if (response) {
+      deferred.resolve(response)
+    }
+  });
+
+  return deferred.promise;
+}
+
+exports.addMemberToList = function (user_id, list_id, muted){
+  var deferred = q.defer();
+  var url = {
+    url:  'https://a.wunderlist.com/api/v1/memberships',
+    method: 'POST',
+    json: {
+      "list_id": list_id,
+      "user_id": user_id,
+      "muted": muted
+    }
+  }
+
+  var options = _.extend(url, keys);
+  request(options, function(err, response, body){
+    if (err) {
+      deferred.reject(err);
+    }
+
+    if (response) {
+      deferred.resolve(response)
+    }
+  });
+
+  return deferred.promise;
+}
+
+exports.removeMember = function (user_id, revision){
+  var deferred = q.defer();
+  var url = {
+    url:  'https://a.wunderlist.com/api/v1/memberships/' + user_id,
+    method: 'DELETE',
+    json: {
+      "revision": revision,
+
+    }
+  }
+
+  var options = _.extend(url, keys);
+  request(options, function(err, response, body){
+    if (err) {
+      deferred.reject(err);
+    }
+
+    if (response) {
+      deferred.resolve(response)
+    }
+  });
+
+  return deferred.promise;
+}
+
+
+
+///////////// END: MEMBERSHIP /////////////
+
+/////////////// TASKCOMMENTS ////////////////
+
+exports.getCommentsList = function (list_id){
+  var deferred = q.defer();
+  var url = {
+    url:  'https://a.wunderlist.com/api/v1/task_comments?list_id=' + list_id,
+    method: 'GET'
+  }
+
+  var options = _.extend(url, keys);
+  request(options, function(err, response, body){
+    if (err) {
+      deferred.reject(err);
+    }
+
+    if (response) {
+      deferred.resolve(response)
+    }
+  });
+
+  return deferred.promise;
+}
+
+exports.getCommentsTask = function (task_id){
+  var deferred = q.defer();
+  var url = {
+    url:  'https://a.wunderlist.com/api/v1/task_comments?task_id=' + task_id,
+    method: 'GET'
+  }
+
+  var options = _.extend(url, keys);
+  request(options, function(err, response, body){
+    if (err) {
+      deferred.reject(err);
+    }
+
+    if (response) {
+      deferred.resolve(response)
+    }
+  });
+
+  return deferred.promise;
+}
+
+
+
+exports.createComment = function (task_id, text){
+  var deferred = q.defer();
+  var url = {
+    url:  'https://a.wunderlist.com/api/v1/task_comments',
+    method: 'POST',
+    json: {
+      "task_id": task_id,
+      "text": text
+    }
+  }
+
+  var options = _.extend(url, keys);
+
+  request(options, function(err, response, body){
+    if (err) {
+      deferred.reject(err);
+    }
+
+    if (response) {
+      deferred.resolve(response)
+    }
+  });
+
+  return deferred.promise;
+}
+
+///////////// END: TASKCOMMENTS /////////////
+
+/////////////// SUBTASKS ////////////////
+
+exports.getSubtaskFromList = function (list_id){
+  var deferred = q.defer();
+  var url = {
+    url:  'https://a.wunderlist.com/api/v1/subtasks?list_id=' + list_id,
+    method: 'GET'
+  }
+
+  var options = _.extend(url, keys);
+  request(options, function(err, response, body){
+    if (err) {
+      deferred.reject(err);
+    }
+
+    if (response) {
+      deferred.resolve(response)
+    }
+  });
+
+  return deferred.promise;
+}
+
+exports.getSubtaskFromTask = function (task_id){
+  var deferred = q.defer();
+  var url = {
+    url:  'https://a.wunderlist.com/api/v1/subtasks?task_id=' + task_id,
+    method: 'GET'
+  }
+
+  var options = _.extend(url, keys);
+  request(options, function(err, response, body){
+    if (err) {
+      deferred.reject(err);
+    }
+
+    if (response) {
+      deferred.resolve(response)
+    }
+  });
+
+  return deferred.promise;
+}
+
+exports.getSubtaskCompletedFromList = function (list_id, completed){
+  var deferred = q.defer();
+  var url = {
+    url:  'https://a.wunderlist.com/api/v1/subtasks?list_id=' + list_id + '&completed=' + completed,
+    method: 'GET'
+  }
+
+  var options = _.extend(url, keys);
+  request(options, function(err, response, body){
+    if (err) {
+      deferred.reject(err);
+    }
+
+    if (response) {
+      deferred.resolve(response)
+    }
+  });
+
+  return deferred.promise;
+}
+
+exports.getSubtaskCompletedFromTask = function (task_id, completed){
+  var deferred = q.defer();
+  var url = {
+    url:  'https://a.wunderlist.com/api/v1/subtasks?task_id=' + task_id + '&completed=' + completed,
+    method: 'GET'
+  }
+
+  var options = _.extend(url, keys);
+  request(options, function(err, response, body){
+    if (err) {
+      deferred.reject(err);
+    }
+
+    if (response) {
+      deferred.resolve(response)
+    }
+  });
+
+  return deferred.promise;
+}
+
+
+exports.createSubstask = function (task_id, title, completed){
+  var deferred = q.defer();
+  var url = {
+    url:  'https://a.wunderlist.com/api/v1/subtasks',
+    method: 'POST',
+    json: {
+      "task_id": task_id,
+      "title": title,
+      "completed": completed
+    }
+  }
+
+  var options = _.extend(url, keys);
+
+  request(options, function(err, response, body){
+    if (err) {
+      deferred.reject(err);
+    }
+
+    if (response) {
+      deferred.resolve(response)
+    }
+  });
+
+  return deferred.promise;
+}
+
+
+exports.deleteSubtask = function (subtask_id, revision){
+  var deferred = q.defer();
+  if (!_.isNumber(arguments[1])) {
+    throw new Error('Argument REVISION require type Number.');
+  }
+
+  var data = {
+    url: 'https://a.wunderlist.com/api/v1/subtasks/' + subtask_id + '?revision=' + revision,
+    method: 'DELETE'
+  }
+
+  var options = _.extend(data, keys);
+
+  request(options, function(err, response, body){
+      if (err) {
+        deferred.reject(err);
+      }
+
+      if (response) {
+        deferred.resolve(response)
+      }
+    });
+
+    return deferred.promise;
+}
+
+
+
+///////////// END: SUBTASKS /////////////
+
+/////////////// NOTES ////////////////
+
+exports.getNoteFromList = function (list_id){
+  var deferred = q.defer();
+  var url = {
+    url:  'https://a.wunderlist.com/api/v1/notes?list_id=' + list_id,
+    method: 'GET'
+  }
+
+  var options = _.extend(url, keys);
+  request(options, function(err, response, body){
+    if (err) {
+      deferred.reject(err);
+    }
+
+    if (response) {
+      deferred.resolve(response)
+    }
+  });
+
+  return deferred.promise;
+}
+
+exports.getNoteFromTask = function (task_id){
+  var deferred = q.defer();
+  var url = {
+    url:  'https://a.wunderlist.com/api/v1/notes?task_id=' + task_id,
+    method: 'GET'
+  }
+
+  var options = _.extend(url, keys);
+  request(options, function(err, response, body){
+    if (err) {
+      deferred.reject(err);
+    }
+
+    if (response) {
+      deferred.resolve(response)
+    }
+  });
+
+  return deferred.promise;
+}
+
+
+exports.createNote = function (task_id, content){
+  var deferred = q.defer();
+  var url = {
+    url:  'https://a.wunderlist.com/api/v1/notes',
+    method: 'POST',
+    json: {
+      "task_id": task_id,
+      "content": content
+    }
+  }
+
+  var options = _.extend(url, keys);
+
+  request(options, function(err, response, body){
+    if (err) {
+      deferred.reject(err);
+    }
+
+    if (response) {
+      deferred.resolve(response)
+    }
+  });
+
+  return deferred.promise;
+}
+
+
+exports.deleteNote = function (note_id, revision){
+  var deferred = q.defer();
+
+  var data = {
+    url: 'https://a.wunderlist.com/api/v1/notes/' + note_id + '?revision=' + revision,
+    method: 'DELETE'
+  }
+
+  var options = _.extend(data, keys);
+
+  request(options, function(err, response, body){
+      if (err) {
+        deferred.reject(err);
+      }
+
+      if (response) {
+        deferred.resolve(response)
+      }
+    });
+
+    return deferred.promise;
+}
