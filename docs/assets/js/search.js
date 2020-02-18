7a542afc7c469620d20f637c35fc97db2b11b74e
Felipe Sousa <felipzbr@gmail.com>
2020-02-18 10:20:04 -0300

    [docs]: initial docs

---
+++ docs/assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"1":"External module"},"rows":[{"id":0,"kind":1,"name":"\"index\"","url":"modules/_index_.html","classes":"tsd-kind-external-module"},{"id":1,"kind":1,"name":"\"index.spec\"","url":"modules/_index_spec_.html","classes":"tsd-kind-external-module"}]};
